6e40435d9fbf92a0b7b2387787598d4c7afe7797
unknown <jou.098olo@gmail.com>
2021-10-03 22:11:56 -0300

    removendo token do mongo

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose')
-
-const baseUrl = 'mongodb+srv://jouiwnl:132Natan123!@cluster0.cfyfl.mongodb.net/bot-discord?retryWrites=true&w=majority';
+const config = require('../configs/config.json')
 
 try {
   // Connect to the MongoDB cluster
   mongoose.connect(
-    baseUrl,
+    config.baseUrlMongo,
     { useNewUrlParser: true, useUnifiedTopology: true },
     () => console.log(" Mongoose is connected"),
   );

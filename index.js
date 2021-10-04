67ab6d3b5692337b2fddba60041767f057ed6402
unknown <jou.098olo@gmail.com>
2021-10-04 14:54:49 -0300

    adicionando cargo de aniversariante/melhorando verificação ao deletar o cargo manualmente

--- index.js
+++ index.js
@@ -2,41 +2,29 @@ const Discord = require("discord.js");
 const config = require('./src/configs/config.json');
 const openServer = require("./src/server/server");
 const Guild = require('./src/model/guilds');
+const BirthdayRole = require("./src/model/birthdayrole");
 const cron = require('cron').CronJob; 
 const { checkMessageAuthor } = require('./src/utils/checkMessageAuthor');
 const { getArgs, getCommand } = require('./src/utils/checkCommand');
 const { add } = require('./src/commands/add')
 const { aniversas } = require('./src/commands/aniversas')
 const { day } = require('./src/commands/day');
-const { addRole } = require("./src/commands/addRole");
+const { addRole } = require('./src/commands/addRole');
+
 
 const token = config.BOT_TOKEN + config.BOT_TOKEN2;
 
 const intents = new Discord.Intents(32767);
 const client = new Discord.Client({ intents });
 
-
-client.on('guildCreate', (guild) => {
+client.on('guildCreate', async (guild) => {
   const guilda = new Guild({ guildId: guild.id, name: guild.name });
-  guilda.save((err, guilda) => {
-    if (err) {
-      return console.log(err);
-    } else {
-      console.log(guilda);
-    }
-  })
+  await guilda.save();
 })
 
-client.on('guildDelete', (guild) => {
-  const guilda = Guild.find({ guildId: guild.id });
-  guilda.deleteOne((err, guilda) => {
-    if (err) {
-      return console.log(err);
-    } else {
-      console.log(guilda);
-      console.log('guilda deletada do banco de dados');
-    }
-  })
+client.on('guildDelete', async (guild) => {
+  const guilda = Guild.findOne({ guildId: guild.id });
+  await guilda.deleteOne();
 })
 
 client.on('messageCreate', (message) => {
@@ -45,14 +33,18 @@ client.on('messageCreate', (message) => {
   if(getCommand(message) == 'adc') {
     add(message, getArgs(message));
   } else if(getCommand(message) == 'aniversas') {
-    aniversas(message, getArgs(message))
+    aniversas(message, getArgs(message));
   } else if(getCommand(message) == 'day') {
-    day(message, getArgs(message))
-  } else if(getCommand(message) == 'addRole') {
-    addRole(message, getArgs(message))
+    day(message, getArgs(message));
+  } else if(getCommand(message) == 'role') {
+    addRole(message, getArgs(message));
   }
 })
 
+client.on('roleDelete', async (role) => {
+  const oldRole = BirthdayRole.findOne({ guildId: role.guild.id, name: role.name });
+  await oldRole.deleteOne();
+})
 
 client.login(token);
 openServer();
--- src/commands/addRole.js
+++ src/commands/addRole.js
@@ -1,18 +1,43 @@
 const BirthdayRole = require('../model/birthdayrole');
 
-exports.addRole = async (message, args) => {
-    
-    const role = new BirthdayRole({
-        guildId: message.guild.id,
-        name: args 
-    });
+exports.addRole = (message, args) => {
+    const roles = message.guild.roles;
 
-    role.save(err => {
-        if(err) {
-            message.reply(`Ocorreu um erro ao salvar o cargo. Tente novamente!`);
-        } else {
-            message.reply(`Cargo salvo no banco de dados`);
-        }
+    roles.fetch().then((allRoles) => {
+        allRoles.map(item => {
+            if (item.name.startsWith('aniver')) {
+                if (item.guild.id == message.guild.id) {
+                    const role = BirthdayRole.findOne({ guildId: message.guild.id })
+                    role.deleteOne((err, role) => {
+                        if (err) {
+                            message.reply(`Erro ao deletar o cargo do banco de dados!`)
+                        } else {
+                            item.delete().then(() => {
+                                message.channel.send(`Já existia um cargo de aniversariante no servidor. Ele foi excluído para a criação de um novo cargo!`)
+                            })
+                        }
+                        
+                    })
+                }
+            }
+        })
     })
 
+    roles.create({ 
+        name: 'aniversariante', 
+        color: 786314,
+        mentionable: true  
+    }).then((role) => {
+        const newRole = new BirthdayRole({ 
+            guildId: role.guild.id,
+            name: role.name
+        })
+        newRole.save(err => {
+            if (err) {
+                message.reply(`Ocorreu um erro ao salvar o cargo!`)
+            } else {
+                message.reply(`O cargo de aniversariante foi criado com êxito!`);
+            }
+        })
+    })
 }

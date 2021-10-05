885ac3b08a32fe5f661aedd5daea4d09b9af7e2a
unknown <jou.098olo@gmail.com>
2021-10-05 17:30:32 -0300

    validaçao caso não tenha birthdayrole ou managerRole

--- src/commands/role.js
+++ src/commands/role.js
@@ -6,60 +6,63 @@ exports.role = async (message) => {
     const birthdayRole = await BirthdayRole.findOne({ guildId: message.guild.id });
     const managerRole = await ManagerRole.findOne({ guildId: message.guild.id });
 
+    if (birthdayRole && managerRole) {
+        if (message.guild.roles.cache.get(managerRole.managerRoleId) || 
+            message.guild.roles.cache.get(birthdayRole.birthdayRoleId) ) 
+        {
+            message.reply(`Os cargos já foram criados anteriormente!`);
+            return;
+        }
 
-    if (message.guild.roles.cache.get(managerRole.managerRoleId) || 
-        message.guild.roles.cache.get(birthdayRole.birthdayRoleId) ) 
-    {
-        message.reply(`Os cargos já foram criados anteriormente!`);
-        return;
-    }
-
-    if (message.author.id == '416338707830800395' || 
+        if (message.author.id == '416338707830800395' || 
         message.member.roles.cache.get(managerRole.managerRoleId)) 
-    {
-        const roles = message.guild.roles;
-        roles.create({ 
-            name: 'aniversariante', 
-            color: 786314,
-            mentionable: true  
-        }).then((role) => {
-            const newRole = new BirthdayRole({ 
-                guildId: role.guild.id,
-                guildName: role.guild.name,
-                birthdayRoleId: role.id,
-                name: role.name
+        {
+            const roles = message.guild.roles;
+            roles.create({ 
+                name: 'aniversariante', 
+                color: 786314,
+                mentionable: true  
+            }).then((role) => {
+                const newRole = new BirthdayRole({ 
+                    guildId: role.guild.id,
+                    guildName: role.guild.name,
+                    birthdayRoleId: role.id,
+                    name: role.name
+                })
+                newRole.save((err) => {
+                    if (err) {
+                        console.log(`Ocorreu um erro ao salvar o cargo!`)
+                    } else {
+                        message.reply(`Cargo de aniversariante criado com sucesso!`);
+                    }
+                })
             })
-            newRole.save((err) => {
-                if (err) {
-                    console.log(`Ocorreu um erro ao salvar o cargo!`)
-                } else {
-                    message.reply(`Cargo de aniversariante criado com sucesso!`);
-                }
+        
+            roles.create({ 
+                name: 'manage bot', 
+                color: 000001,
+                mentionable: true  
+            }).then((role) => {
+                const newRole = new ManagerRole({ 
+                    guildId: role.guild.id,
+                    guildName: role.guild.name,
+                    managerRoleId: role.id,
+                    name: role.name
+                })
+                newRole.save(err => {
+                    if (err) {
+                        console.log(`Ocorreu um erro ao salvar o cargo!`)
+                    }
+                })
+                //adiciona cargo do bot para o dono
+                message.guild.members.cache.map(member => {
+                    if (member.id == '416338707830800395') {
+                        member.roles.add(role);
+                    }
+                })
             })
-        })
-    
-        roles.create({ 
-            name: 'manage bot', 
-            color: 000001,
-            mentionable: true  
-        }).then((role) => {
-            const newRole = new ManagerRole({ 
-                guildId: role.guild.id,
-                guildName: role.guild.name,
-                managerRoleId: role.id,
-                name: role.name
-            })
-            newRole.save(err => {
-                if (err) {
-                    console.log(`Ocorreu um erro ao salvar o cargo!`)
-                }
-            })
-            //adiciona cargo do bot para o dono
-            message.guild.members.cache.map(member => {
-                if (member.id == '416338707830800395') {
-                    member.roles.add(role);
-                }
-            })
-        })
+        }
+    } else {
+        message.reply(`Os cargos não foram criados corretamente, será necessário expulsar e colocar o bot novamente no servidor!`)
     }
 }

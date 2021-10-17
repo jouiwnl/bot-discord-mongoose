ef75901d52d73c07ce0ab38c857b510eb9648291
unknown <jou.098olo@gmail.com>
2021-10-17 00:09:51 -0300

    atualizando gitignore

--- configs/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  BOT_TOKEN : 'ODc1NzM0MzIyNTY3NTg1ODAy.',
-  BOT_TOKEN2 : 'YRZ1JA.nPq2mXAA7Ih0Kg8GRZEx3I5rIwk',
-  baseUrlMongo : 'mongodb+srv://jouiwnl:vaisefuder@cluster0.cfyfl.mongodb.net/bot-discord?retryWrites=true&w=majority'
-};
-
-export default config;

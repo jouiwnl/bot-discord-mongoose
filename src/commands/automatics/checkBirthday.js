22af25bc10b7621f67c5ccc9bfb5eb41989d3525
Joao Henrique Leonor Martins <BETHA\joao.martins@7vrllh3.betha.com.br>
2021-10-24 23:00:33 -0300

    prod

--- src/commands/automatics/checkBirthday.js
+++ src/commands/automatics/checkBirthday.js
@@ -5,7 +5,6 @@ import buildMessage from '../../embeds/happyBirthday.js';
 import getData from '../../utils/data.js';
 
 const checkbirthday = async (client) => {
-  var counting = 0;
   client.guilds.cache.map(async guild => {
 
     const birthdayRole = await BirthdayRole.findOne({ guildId: guild.id });
@@ -14,35 +13,25 @@ const checkbirthday = async (client) => {
 
     usuarios.map(async usuario => { 
       if (usuario.birthday == getData()) {
-        counting = counting + 1;
         guild.members.cache.map(item => {
           if(item.id == usuario.userId) {
-            const canal = guild.channels.cache.get(channel.id);
-            if(counting > 1) {
-              var desc = `-${usuario.username}`;
-              var descFinal = `${desc}-${usuario.username}`;
-              canal.setName(`〚🤖〛parabéns${descFinal}`);
-            } else if (counting == 1) {
-              canal.setName(`〚🤖〛parabéns-${usuario.username}`);
-            }   
+            const canal = guild.channels.cache.get(channel.id);  
             canal.send({ 
               embeds: [buildMessage(usuario, guild.channels.cache.get(channel.id))] 
             });
             item.roles.add(guild.roles.cache.get(birthdayRole.birthdayRoleId));
           }
         });
-      } else if (usuario.birthday != getData()) {
-        const canal = guild.channels.cache.get(channel.id);
-        canal.setName('〚🤖〛parabéns');
-        
+      }
+
+      if (usuario.birthday != getData()) {
         guild.members.cache.map(item => {
           if(item.id == usuario.userId && item.roles.cache.get(birthdayRole.birthdayRoleId)) {
             item.roles.remove(guild.roles.cache.get(birthdayRole.birthdayRoleId));
           }
         });
       }
-    });
-        
+    }); 
   });
 };
 

8e96082ee1626b26948d8559e3d37acf05d55169
jouiwnl <jou.098olo@gmail.com>
2022-01-05 18:03:30 -0300

    joga instanciaçao da data para dentro da funçao

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,10 +1,11 @@
-var dataAtualCompleta = '';
-var dataAtual = new Date();
-
-var dia = dataAtual.getDate().toLocaleString('pt-BR');
-var mes = (dataAtual.getMonth() + 1).toLocaleString('pt-BR');
-    
 const getData = () => {
+
+  var dataAtualCompleta = '';
+  var dataAtual = new Date();
+
+  var dia = dataAtual.getDate().toLocaleString('pt-BR');
+  var mes = (dataAtual.getMonth() + 1).toLocaleString('pt-BR');
+
   if (mes.length > 1 && dia.length > 1) {
     dataAtualCompleta = `${dia}/${mes}`;  
   } else if (mes.length > 1 && dia.length == 1) {
